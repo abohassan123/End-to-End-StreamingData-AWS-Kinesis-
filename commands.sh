ef1743cbe9e6039c97ffd6f8cb58cfa674d07b71
Mohamed Abohassan <mohamed.ah.abohassan@gmail.com>
2022-11-15 04:35:00 +0200

    kinesis

---
+++ commands.sh
@@ -0,0 +1,26 @@
+sudo yum update -y
+sudo yum install git -y
+sudo yum -y install python-pip
+sudo pip install pytz
+sudo pip install numpy
+sudo pip install faker
+sudo pip install tzlocal
+-------------------------------------------
+git clone https://github.com/kiritbasu/Fake-Apache-Log-Generator.git
+-------------------------------------------
+sudo yum install –y aws-kinesis-agent # install aws-kinesis-agent 
+-------------------------------------------
+cd /etc/aws-kinesis/ #the pass to edit the agent confg file
+------------------------------------------
+# name of confg is : agent.json
+-------------------------------
+cd /var/log/aws-kinesis-agent # agent dir
+------------------------------------------
+sudo python /Fake-Apache-Log-Generator/apache-fake-log-gen.py -n 0 -o LOG & # run python file to get logs data
+------------------------------------------
+sudo service aws-kinesis-agent start #Start the agent manually
+-------------------------------------------
+
+
+
+
---
+++ stream_pump.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE STREAM "DESTINATION_SQL_STREAM"
+(datetime TIMESTAMP, status INTEGER, statusCount
+INTEGER);
+-- Create pump to insert into output
+CREATE OR REPLACE PUMP "STREAM_PUMP" AS
+INSERT INTO "DESTINATION_SQL_STREAM"
+-- Select all columns from source stream
+SELECT
+STREAM ROWTIME as datetime,
+"response" as status,
+COUNT(*) AS statusCount
+FROM "SOURCE_SQL_STREAM_001"
+GROUP BY
+"response",
+FLOOR(("SOURCE_SQL_STREAM_001".ROWTIME -TIMESTAMP
+'1970-01-01 00:00:00') minute / 1 TO MINUTE);
